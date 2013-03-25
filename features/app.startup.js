213afcb5df22d781d9043a63ab2efcafc543fd91
James White <james@llamasantos.com>
2013-03-25 10:54:13 -0600

    Updated the project to take advantage of new features in nodeject

--- features/app.startup.js
+++ features/app.startup.js
@@ -1,39 +1,51 @@
 // -- Browser Script
 (function (win, $, _, Hogan, tmpls){
 
+    /* We make strong references like TodoOperation for clarity's sake.
+    * Normally in your application you would require the dependencies directly
+    * in the container configuration for terseness.  Ex.
+    *
+    * container.define({ name : "MyOperation", type : require("./path/to/operation.js").create, category : "operations" });
+    * */
     var TodoOperation = require("./todo/todoOperations.js"),
         TodoController = require("./todo/todoController.js"),
         TodoPresenter = require("./todo/todoPresenter.js"),
         app = require("./app.js"),
         Nodeject = require("nodeject");
 
+    var wrapGlobal = function (obj) { return function () { return obj ; }};
+    var wrap = function (name) { return function (obj) { return obj[name]; }};
+
     var container = new Nodeject();
-    container.define({ name : "jquery",    type : function () { return $; }, singleton: true })
-        .define({ name : "_",              type : function () { return _; }, singleton : true })
-        .define({ name : "templates",      type : function () { return tmpls; }, singleton: true })
+    container.define({ name : "jquery",    type : wrapGlobal($), singleton: true })
+        .define({ name : "_",              type : wrapGlobal(_), singleton : true })
+        .define({ name : "templates",      type : wrapGlobal(tmpls), singleton: true })
         .define({ name : "app",            type : app.create, singleton : true, deps: ["templates"] })
-        .define({ name : "bus",            type : function (app) { return app.bus; }, singleton: true, deps: ["app"] })
-        .define({ name : "store",          type : function (app) { return app.store; }, singleton: true, deps: ["app"] })
-        .define({ name : "todoOperation",  type : TodoOperation.create, singleton: true, deps: ["_", "store"] })
-        .define({ name : "todoController", type : TodoController.create, deps: ["_", "bus", "todoOperation"] })
-        .define({ name : "todoPresenter",  type : TodoPresenter.create, deps: ["jquery", "_", "bus", "templates"]})
-        .define({ name : "bootstrap",      type: function (app, presenter, controller){
-            app.presenters = {
-                todo: presenter
-            };
+        .define({ name : "bus",            type : wrap("bus"), singleton: true, deps: ["app"] })
+        .define({ name : "store",          type : wrap("store"), singleton: true, deps: ["app"] })
+        .define({ name : "todoOperation",  type : TodoOperation.create, category : "operations", singleton: true, deps: ["_", "store"] })
+        .define({ name : "todoController", type : TodoController.create, category : "controllers", deps: ["_", "bus", "todoOperation"] })
+        .define({ name : "todoPresenter",  type : TodoPresenter.create, category : "presenters", deps: ["jquery", "_", "bus", "templates"]})
+        .define({ name : "startup",      type: function (app){
+            // Initialize the Presenters
+            app.presenters = container.define({ category : "presenters", format : "literal" });
 
-            app.controllers = {
-                todo: controller
-            };
+            // Initialize the controllers
+            app.controllers = container.define({ category : "controllers", format : "literal" });
 
+            // Assign the container
+            win.app.container = container;
+
+            // Create a noop function for general purpose
+            win.app.noop = function (){};
+
+            // Return the initialized app
             return app;
-        }, deps: ["app", "todoPresenter", "todoController"]});
+        }, deps: ["app"]});
 
 
     if (!('app' in win)){
-        win.app = container.resolve("bootstrap");
-        win.app.container = container;
-        win.app.noop = function (){};
+        win.app = container.resolve("startup");
     }
 
     if (!('require' in win)){
